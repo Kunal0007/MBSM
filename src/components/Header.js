dd50c9806fe8e9a46d710a9481aed7d1aad586a2
omkar-2002 <omkarsalapurkar2002@gmail.com>
2022-10-17 01:23:41 +0530

    initialize firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,23 +9,33 @@ const Header = () => {
     <div className="header1">
       {/* <div className="imgLogo">
       <a href="http://bsmbharat.org/"> */}
-        <img
-          style={{ height: 80,margin:10 }}
-          src="http://bsmbharat.org/wp-content/uploads/elementor/thumbs/rfr-logo-pdxx8z7xf4n5q53w1tjmsomv3eyw7k23bnbw19t5vw.png"
-          title="rfr-logo"
-          alt="rfr-logo"
-        />
+      <img
+        style={{ height: 80, margin: 10 }}
+        src="http://bsmbharat.org/wp-content/uploads/elementor/thumbs/rfr-logo-pdxx8z7xf4n5q53w1tjmsomv3eyw7k23bnbw19t5vw.png"
+        title="rfr-logo"
+        alt="rfr-logo"
+      />
       {/* </a>
       </div> */}
-    <div className="titleHeader">
-      <h2>महाराष्ट्र भारतीय शिक्षण मंडळ</h2>
-      <h5>Maharashtra Bhartiya Shiksan Mandal</h5>
-    </div>
-    <div className="address">
-<p><FontAwesomeIcon color="#86003e" size="x" icon={faLocationDot} /> : 189 Ashish Charkop Mumbai-400067</p>
-<p> <FontAwesomeIcon color="#86003e" size="x" icon={faPhone} /> : 02228691908/1503</p>
-<p><FontAwesomeIcon color="#86003e" size="x" icon={faMailForward} /> : mbsmmandal@gmail.com</p>
-    </div>
+      <div className="titleHeader">
+        <h2>महाराष्ट्र भारतीय शिक्षण मंडळ</h2>
+        <h5>Maharashtra Bhartiya Shiksan Mandal</h5>
+      </div>
+      <div className="address">
+        <p>
+          <FontAwesomeIcon color="#86003e" size="x" icon={faLocationDot} /> :
+          189 Ashish Charkop Mumbai-400067
+        </p>
+        <p>
+          {" "}
+          <FontAwesomeIcon color="#86003e" size="x" icon={faPhone} /> :
+          02228691908/1503
+        </p>
+        <p>
+          <FontAwesomeIcon color="#86003e" size="x" icon={faMailForward} /> :
+          mbsmmandal@gmail.com
+        </p>
+      </div>
     </div>
   );
 };
