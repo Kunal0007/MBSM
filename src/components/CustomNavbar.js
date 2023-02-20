1da2bdefe9033a0187cfa50c56ad019ad8310e9f
omkar-2002 <omkarsalapurkar2002@gmail.com>
2023-02-21 00:08:10 +0530

    Added organizational structure

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -7,31 +7,32 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import "../components/CustomNavbarStyle.css";
 
 const CustomNavbar = () => {
-
   return (
     <div>
-      <Navbar collapseOnSelect
+      <Navbar
+        collapseOnSelect
         bg="dark"
         variant="dark"
         expand="lg"
         style={{
           backgroundImage: "linear-gradient(180deg, #D8381A 3%, #BF0B5E 91%)",
-          height: 60
+          height: 60,
         }}
       >
         <Container>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto" style={{
-              backgroundImage: "linear-gradient(180deg, #D8381A 3%, #BF0B5E 91%)",
-            }}>
+            <Nav
+              className="me-auto"
+              style={{
+                backgroundImage:
+                  "linear-gradient(180deg, #D8381A 3%, #BF0B5E 91%)",
+              }}
+            >
               <Nav.Link className="navItem" href="/">
                 Home
               </Nav.Link>
-              <NavDropdown
-                title="About Us"
-                id="collasible-nav-dropdown"
-              >
+              <NavDropdown title="About Us" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="/aboutus/introduction">
                   Introduction
                 </NavDropdown.Item>
@@ -46,17 +47,23 @@ const CustomNavbar = () => {
                 title="Organiztional Structure"
                 id="collasible-nav-dropdown"
               >
-                <NavDropdown.Item href="#action/3.1">Executive committee</NavDropdown.Item>
+                <NavDropdown.Item href="/organizational-structure">
+                  Executive committee
+                </NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown
-                title="Publication"
-                id="collasible-nav-dropdown"
-              >
-                <NavDropdown.Item href="/publication/magzines"> Magzines </NavDropdown.Item>
+              <NavDropdown title="Publication" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/publication/magzines">
+                  {" "}
+                  Magzines{" "}
+                </NavDropdown.Item>
                 <NavDropdown.Item href="/publication/question-papers">
                   Question papers for schools
                 </NavDropdown.Item>
-                <NavDropdown title="Books" id="collasible-nav-dropdown" style={{"padding": "4px 8px"}}>
+                <NavDropdown
+                  title="Books"
+                  id="collasible-nav-dropdown"
+                  style={{ padding: "4px 8px" }}
+                >
                   <NavDropdown.Item href="/publication/prakashan-books">
                     Prakashan Books
                   </NavDropdown.Item>
@@ -71,6 +78,9 @@ const CustomNavbar = () => {
                   Research Papers
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link className="navItem" href="/activity">
+                Activity
+              </Nav.Link>
               <Nav.Link className="navItem" href="/gallery/photogallery">
                 Photo Gallery
               </Nav.Link>
